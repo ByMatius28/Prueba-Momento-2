9b55e1657e9821da822a40eaad27e298d5eabc23
ByMatius28 <amaguilera@sudamericano.edu.ec>
2023-12-21 10:18:51 -0500

    wenas

--- Ejercicio2/ejercicio2.js
+++ Ejercicio2/ejercicio2.js
@@ -2,17 +2,15 @@
 //A a partir del array ages=[15, 50, 45] agregue su edad. 
 //Usando un for con un if, muestre en pantalla  solo las edades mayores a 25
 
-let numeros=[15,50,45]
+let numeros=[15,45,50]
 
 numeros.unshift(18)
 
 for(let i = 0; i < numeros.length; i++){
-    alert( `${numeros[i]}`);
     if(numeros >= 25){
-        alert(numeros)
+     alert(numeros[i]);
     }
-    else{
-        alert(numeros)
-    }
+   
+   
 }
 
